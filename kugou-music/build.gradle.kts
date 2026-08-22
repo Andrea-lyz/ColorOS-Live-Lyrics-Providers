723051a5df8391ff39bfdead9f7365fff96ca526
Andrea-lyz <Andrea-lyz@users.noreply.github.com>
2026-08-22 17:02:16 +0200

    Bump KuGou provider to 1.2.0

--- kugou-music/build.gradle.kts
+++ kugou-music/build.gradle.kts
@@ -21,8 +21,8 @@ configure<ApplicationExtension> {
         applicationId = "io.github.proify.lyricon.kgprovider"
         minSdk = 27
         targetSdk = rootProject.extra.get("targetSdkVersion") as Int
-        versionCode = 14
-        versionName = "1.1.9"
+        versionCode = 15
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
